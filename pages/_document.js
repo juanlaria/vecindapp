8f6a894c9b82c6664643ac5b01c0d4eb60033681
unknown <juan.laria@gmail.com>
2020-11-07 19:57:36 -0300

    header and styles stuff

---
+++ pages/_document.js
@@ -0,0 +1,43 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { extractCritical } from 'emotion-server';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const page = renderPage();
+    const styles = extractCritical(page.html);
+    return { ...page, ...styles };
+  }
+
+  constructor(props) {
+    super(props);
+    const lang = "es";
+    this.lang = lang;
+  }
+
+  render() {
+    return (
+      <html lang={this.lang} dir="ltr">
+        <meta name="application-name" content="Vecindapp" />
+        <meta name="theme-color" content="#FFFFFF" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/180x180.png"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <Head>
+          <style
+            data-emotion-css={this.props.ids.join(' ')}
+            dangerouslySetInnerHTML={{ __html: this.props.css }}
+          />
+        </Head>
+        <body>
+            <header>Header</header>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
