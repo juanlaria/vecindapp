ee2600b17f6b8b9ff086e85cab7e186701296930
unknown <juan.laria@gmail.com>
2020-11-08 14:32:10 -0300

    linters

---
+++ .babelrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    presets: [
+        [
+        'next/babel',
+        {
+            'preset-env': {},
+        },
+        ],
+    ],
+    plugins: ['babel-plugin-emotion']
+};
